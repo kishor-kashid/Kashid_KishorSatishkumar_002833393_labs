02bfb8e29240f0b9a3ae5e4f368689be8cbcb5ff
Kishor Satishkumar Kashid <kashid.k@northeastern.edu>
2023-09-15 17:38:18 -0400

    First commit

---
+++ Lab_1/src/Model/Product.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Product {
+    
+    private String name;
+    private String description;
+    private String avialableNum;
+    private String price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAvialableNum() {
+        return avialableNum;
+    }
+
+    public void setAvialableNum(String avialableNum) {
+        this.avialableNum = avialableNum;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+    
+    
+}
